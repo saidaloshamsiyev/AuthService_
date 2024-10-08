8508cf3d8d72fa7d92c2508101318b4be924f77e
said4ik <muxammadaminartikov@gmail.com>
2024-10-08 20:56:18 +0500

    docker-compose.yaml config full end

--- src/main/java/org/example/authservice/domain/entity/UserEntity.java
+++ src/main/java/org/example/authservice/domain/entity/UserEntity.java
@@ -2,15 +2,9 @@ package org.example.authservice.domain.entity;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
+import lombok.*;
+
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -19,18 +13,14 @@ import java.util.UUID;
 @Setter
 @Entity(name = "users")
 @Builder
-public class UserEntity extends BaseEntity implements UserDetails {
+public class UserEntity extends BaseEntity {
     private String username;
     private String password;
     private String email;
     private String picturePath;
     private UUID videoId;
     private UUID subscriptionId;
-    @Enumerated(EnumType.STRING)
-    private UserRole role;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_"+role.name()));
-    }
+
+
 }
