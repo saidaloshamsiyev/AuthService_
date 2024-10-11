5c480f955d8f302a214bb0adf65ae280f70b7746
said4ik <muxammadaminartikov@gmail.com>
2024-10-11 16:43:28 +0500

    iwla

--- src/main/java/org/example/authservice/service/service/user/UserService.java
+++ src/main/java/org/example/authservice/service/service/user/UserService.java
@@ -2,10 +2,11 @@ package org.example.authservice.service.service.user;
 
 
 
-import metube.com.dto.request.LoginDTO;
-import metube.com.dto.response.UserResponse;
+
+import org.example.authservice.domain.request.LoginDTO;
 import org.example.authservice.domain.request.UserRequest;
 import org.example.authservice.domain.response.JwtResponse;
+import org.example.authservice.domain.response.UserResponse;
 
 
 import java.util.List;
