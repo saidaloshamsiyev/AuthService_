e7704fe74553ec057d4674039f1d553f7905efb6
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-01 12:32:25 +0500

    auth

--- src/main/java/org/example/authservice/config/SecurityConfig.java
+++ src/main/java/org/example/authservice/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,7 +17,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
-
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
