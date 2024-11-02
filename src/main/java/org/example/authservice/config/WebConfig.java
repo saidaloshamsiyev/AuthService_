6a223b6069a0c0c951f06bbcd9fb98de3a90d30d
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-02 18:36:23 +0500

    auth

--- src/main/java/org/example/authservice/config/WebConfig.java
+++ src/main/java/org/example/authservice/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Barcha so'rovlar uchun
-                .allowedOrigins("http://localhost:3001","ttp://localhost:3000") // Faqat localhost:3001 manzilidan kelgan so'rovlar
+                .allowedOrigins("http://localhost:3001", "http://localhost:3000", "http://159.65.119.240:8080") // Faqat belgilangan manzilardan kelgan so'rovlar
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Qabul qilinadigan HTTP usullari
                 .allowedHeaders("*"); // Qabul qilinadigan sarlavhalar
     }
