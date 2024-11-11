bc3c76e105f72185aa259015f55dfff7cf5e1273
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-11 10:56:43 +0500

    auth

--- src/main/java/org/example/authservice/config/SecurityConfig.java
+++ src/main/java/org/example/authservice/config/SecurityConfig.java
@@ -25,8 +25,8 @@ public class SecurityConfig {
             "/api/auth/register",
             "/api/auth/swagger-ui/**",
             "/api/auth/v3/api-docs/**",
-            "/v3/api-docs/",
-            "/swagger-ui/",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
             "/swagger-ui.html"};
 
     @Bean
